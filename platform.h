ea97c3af08f60afee95b5e4d652792d0c804ef31
EddieRich <ehandrich@gmail.com>
2026-05-27 15:24:31 -0400

    cleanup build

--- platform.h
+++ platform.h
@@ -12,10 +12,10 @@ typedef enum ePenThickness
 	PT_THICK
 } PenThickness;
 
-void DrawRectangle(float left, float top, float right, float bottom);
-void DrawCircle(float center_x, float center_y, float radius);
-void DrawLine(float x1, float y1, float x2, float y2);
-void DrawArc(float center_x, float center_y, float a, float b, float start_angle, float end_angle);
+void DrawRectangle(float left, float top, float right, float bottom, PenThickness pt);
+void DrawCircle(float center_x, float center_y, float radius, PenThickness pt);
+void DrawLine(float x1, float y1, float x2, float y2, PenThickness pt);
+void DrawArc(float center_x, float center_y, float a, float b, float start_angle, float end_angle, PenThickness pt);
 
 #define _PI_ 3.1415926535897932384626433832795
 #define deg2rad(d) ((float)(d * _PI_ / 180.0))
--- windows_platform.c
+++ windows_platform.c
@@ -10,14 +10,21 @@
 static HWND hwnd;
 static HDC hdc;
 static HPEN pen[3];
+static PenThickness last_pt = PT_THIN;
 
 void FillRectangle(float left, float top, float right, float bottom)
 {
 	Rectangle(hdc, client_x(left), client_y(top), client_x(right), client_y(bottom));
 }
 
-void DrawRectangle(float left, float top, float right, float bottom)
+void DrawRectangle(float left, float top, float right, float bottom, PenThickness pt)
 {
+	if (pt != last_pt)
+	{
+		last_pt = pt;
+		SelectObject(hdc, pen[pt]);
+	}
+
 	Rectangle(hdc, client_x(left), client_y(top), client_x(right), client_y(bottom));
 }
 
@@ -29,18 +36,30 @@ void DrawRectangle(float left, float top, float right, float bottom)
 // 	SelectObject(hdc, brush[NONE]);
 // }
 
-void DrawCircle(float center_x, float center_y, float radius)
+void DrawCircle(float center_x, float center_y, float radius, PenThickness pt)
 {
+	if (pt != last_pt)
+	{
+		last_pt = pt;
+		SelectObject(hdc, pen[pt]);
+	}
+
 	Ellipse(hdc, client_x(center_x - radius), client_y(center_y - radius), client_x(center_x + radius), client_y(center_y + radius));
 }
 
-void DrawLine(float x1, float y1, float x2, float y2)
+void DrawLine(float x1, float y1, float x2, float y2, PenThickness pt)
 {
+	if (pt != last_pt)
+	{
+		last_pt = pt;
+		SelectObject(hdc, pen[pt]);
+	}
+
 	MoveToEx(hdc, client_x(x1), client_y(y1), NULL);
 	LineTo(hdc, client_x(x2), client_y(y2));
 }
 
-void DrawArc(float center_x, float center_y, float a, float b, float start_angle, float end_angle)
+void DrawArc(float center_x, float center_y, float a, float b, float start_angle, float end_angle, PenThickness pt)
 {
 	int x1 = client_x(center_x - a / 2.0f);
 	int y1 = client_y(center_y - b / 2.0f);
@@ -53,6 +72,12 @@ void DrawArc(float center_x, float center_y, float a, float b, float start_angle
 	sincosf(end_angle, &fsin, &fcos);
 	int x4 = client_x(center_x + fcos * a);
 	int y4 = client_y(center_y - fsin * b);
+
+	if (pt != last_pt)
+	{
+		last_pt = pt;
+		SelectObject(hdc, pen[pt]);
+	}
 
 	Arc(hdc, x1, y1, x2, y2, x3, y3, x4, y4);
 }
@@ -68,15 +93,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 		FillRectangle(0.0, 0.0, canvas_width(), canvas_height());
 		SelectObject(hdc, GetStockObject(NULL_BRUSH));
 
-		SelectObject(hdc, pen[PT_THIN]);
-		DrawRectangle(2.0, 2.0, 3.0, 3.0);
-		DrawCircle(2.5, 2.5, 0.25);
-		DrawLine(1.0, 1.0, 9.5, 1.0);
-		DrawArc(7.0, 3.0, 2.0, 1.0, deg2rad(45), deg2rad(135));
+		DrawRectangle(2.0, 2.0, 3.0, 3.0, PT_THIN);
+		DrawCircle(2.5, 2.5, 0.25, PT_NORMAL);
+		DrawLine(1.0, 1.0, 9.5, 1.0, PT_THICK);
+		DrawArc(7.0, 3.0, 2.0, 1.0, deg2rad(45), deg2rad(135), PT_NORMAL);
 
-		DrawLine(6.8, 6.9, 7.2, 6.9);
-		SelectObject(hdc, pen[PT_NORMAL]);
-		DrawArc(7.0, 8.0, 1.0, 2.0, deg2rad(70), deg2rad(110));
+		DrawLine(6.8, 6.9, 7.2, 6.9, PT_THIN);
+		DrawArc(7.0, 8.0, 1.0, 2.0, deg2rad(70), deg2rad(110), PT_THIN);
 
 		SelectObject(hdc, oldbrush);
 		SelectObject(hdc, oldpen);
@@ -106,8 +129,17 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 			return 1LL;
 		}
 	}
+	else if (msg == WM_CREATE)
+	{
+		pen[PT_THIN] = CreatePen(PS_SOLID, 1, RGB(0,0,0));
+		pen[PT_NORMAL] = CreatePen(PS_SOLID, 3, RGB(0,0,0));
+		pen[PT_THICK] = CreatePen(PS_SOLID, 5, RGB(0,0,0));
+	}
 	else if (msg == WM_DESTROY)
 	{
+		DeleteObject(pen[PT_THIN]);
+		DeleteObject(pen[PT_NORMAL]);
+		DeleteObject(pen[PT_THICK]);
 		PostQuitMessage(0);
 		return 1LL;
 	}
@@ -143,19 +175,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-void initGDI(void)
-{
-	pen[PT_THIN] = CreatePen(PS_SOLID, 1, RGB(0,0,0));
-	pen[PT_NORMAL] = CreatePen(PS_SOLID, 3, RGB(0,0,0));
-	pen[PT_THICK] = CreatePen(PS_SOLID, 5, RGB(0,0,0));
-}
-
-void releaseGDI(void)
-{
-	for (int i = 0; i < 3; i++)
-		DeleteObject(pen[i]);
-}
-
 int main()
 {
 	HINSTANCE instance = GetModuleHandle(0);
@@ -179,7 +198,6 @@ int main()
 		return -1;
 	}
 
-	initGDI();
 	set_canvas_size(10.5, 8.0);
 	ShowWindow(hwnd, SW_NORMAL);
 
@@ -190,6 +208,5 @@ int main()
 		DispatchMessage(&msg);
 	}
 
-	releaseGDI();
 	return 0;
 }
