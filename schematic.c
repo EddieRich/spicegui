f0487a1f4f1b1e4d5a45217993e28a5e96a71f2b
EddieRich <ehandrich@gmail.com>
2026-05-28 15:20:36 -0400

    add schematic files

---
+++ schematic.c
@@ -0,0 +1,20 @@
+#include "platform.h"
+#include "schematic.h"
+
+void create_schematic(float* pwidth, float*pheight)
+{
+	*pwidth = 11.0;
+	*pheight = 8.5;
+
+	// create whatever is needed for a schematic
+}
+
+void render_schematic()
+{
+	DrawRectangle(2.0, 2.0, 3.0, 3.0, PT_THIN);
+	DrawCircle(2.5, 2.5, 0.25, PT_NORMAL);
+	DrawLine(1.0, 1.0, 9.5, 1.0, PT_THICK);
+	DrawArc(7.0, 3.0, 2.0, 1.0, deg2rad(45), deg2rad(135), PT_NORMAL);
+	DrawLine(6.8, 6.9, 7.2, 6.9, PT_THIN);
+	DrawArc(7.0, 8.0, 1.0, 2.0, deg2rad(70), deg2rad(110), PT_THIN);
+}
---
+++ schematic.h
@@ -0,0 +1,7 @@
+#ifndef __SCHEMATIC_H__
+#define __SCHEMATIC_H__
+
+void create_schematic(float* pwidth, float* pheight);
+void render_schematic();
+
+#endif // __SCHEMATIC_H__
